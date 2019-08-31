ddcffee17b1ce86b352d0a6bb6c6ab9ca4755ff8
Lucas Sloan <lucas.sloan@gmail.com>
2019-08-31 16:07:46 -0700

    Add data augmentation and shuffling.

--- multi_frame_predictor.py
+++ multi_frame_predictor.py
@@ -11,7 +11,13 @@ def parse_record(tfrecord):
     proto = tf.parse_single_example(tfrecord, image_feature_description)
 
     image_decoded = tf.image.decode_jpeg(proto['image_raw'], channels=3)
-    image_float = tf.image.convert_image_dtype(image_decoded, tf.float32)
+    image_flipped = tf.image.random_flip_left_right(image_decoded)
+    image_hue = tf.image.random_hue(image_flipped, 0.08)
+    image_sat = tf.image.random_saturation(image_hue, 0.6, 1.6)
+    image_bright = tf.image.random_brightness(image_sat, 0.05)
+    image_contrast = tf.image.random_contrast(image_bright, 0.7, 1.3)
+
+    image_float = tf.image.convert_image_dtype(image_contrast, tf.float32)
 
     return image_float, proto['label']
 
@@ -20,7 +26,8 @@ def load_dataset(filename):
 
     dataset = raw_dataset.map(parse_record)
     dataset = dataset.apply(tf.contrib.data.sliding_window_batch(5))
-    dataset = dataset.batch(10)
+    dataset = dataset.shuffle(1000)
+    dataset = dataset.batch(20)
     return dataset
 
 training_dataset = load_dataset("D:\\speedchallenge\\temporal\\train.tfrecords")
