99f15ce2433723424653c2288712648d27da6255
Lucas Sloan <lucas.sloan@gmail.com>
2019-11-07 09:01:51 -0800

    Create a comma ai dataset with full pose information.

--- extract_commaai_2k19.py
+++ extract_commaai_2k19.py
@@ -3,41 +3,107 @@ import cv2
 from extract import create_records, write_records
 import glob
 from multiprocessing import Pool
+import camera
+import orientation
+import cv2
+import tensorflow as tf
+import random
 
-def extract_segment(segment_info):
-    num, path = segment_info
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _float_list_feature(values):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+
+# takes in 2 ecef quaternions and returns the euler angle between them
+def relative_orientation(ecef_quat_1, ecef_quat_2):
+    ecef_mat_1 = orientation.quat2rot(ecef_quat_1)
+    ecef_mat_2 = orientation.quat2rot(ecef_quat_2)
+
+    relative_mat = np.matmul(ecef_mat_2, np.transpose(ecef_mat_1))
+
+    return orientation.rot2euler(relative_mat)
+
+def extract_segment(path):
     frame_times = np.load(path + '/global_pose/frame_times')
     frame_velocities =  np.linalg.norm(np.load(path + '/global_pose/frame_velocities'),axis=1)
     frame_velocities = list(map(lambda x: x.item(), frame_velocities))
 
-    for time, velocity in zip(frame_times[:20], frame_velocities[:20]):
-        print(time, type(velocity))
 
     cam = cv2.VideoCapture(path + "/video.hevc")
+    frames = []
 
-    print(hash(path))
+    while(True):
+        ret, frame = cam.read()
 
-    examples = create_records(cam, frame_velocities)
+        if ret:
+            frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
+            ret, jpg = cv2.imencode(".jpg", frame)
+            if not ret:
+                raise Exception("couldn't encode the image")
 
-    print(len(examples))
+            frame_bytes = jpg.tostring()
+            frames.append(frame_bytes)
 
-    evens = examples[::2]
-    write_records(evens, "/mnt/d/commaai/segments/segment_{}_evens.tfrecord".format(num))
-
-    odds = examples[1::2]
-    write_records(odds, "/mnt/d/commaai/segments/segment_{}_odds.tfrecord".format(num))
+        else:
+            break
 
 
+    frame_positions = np.load(path + '/global_pose/frame_positions')
+    frame_orientations = np.load(path + '/global_pose/frame_orientations')
 
-segment_dirs = glob.glob("/mnt/d/commaai/comma2k19/*/*/*")
+    assert len(frames) == len(frame_positions) == len(frame_orientations)
 
-print(len(segment_dirs))
+    previous_position = frame_positions[0]
+    previous_orientation = frame_orientations[0]
+    previous_frame = frames[0]
 
-enumerated_dirs = [x for x in enumerate(segment_dirs)]
+    examples = []
+    for position, orientation, frame, speed in zip(frame_positions[1:], frame_orientations[1:], frames[1:], frame_velocities[1:]):
+        rel_position = camera.device_from_ecef(previous_position, previous_orientation, position)
+        rel_orientation = relative_orientation(previous_orientation, orientation)
 
-print(enumerated_dirs[:10])
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'frame_one': _bytes_feature(previous_frame),
+            'frame_two': _bytes_feature(frame),
+            'position': _float_list_feature(rel_position),
+            'orientation': _float_list_feature(rel_orientation),
+            'speed': _float_feature(speed),
+        }))
+        examples.append(example.SerializeToString())
 
-with Pool(32) as p:
-    p.map(extract_segment, enumerated_dirs)
+        previous_position, previous_orientation, previous_frame = position, orientation, frame
 
-# extract_segment((1, "/mnt/d/commaai/comma2k19/Chunk_1/b0c9d2329ad1606b_2018-07-27--06-03-57/3"))
+    return examples
+
+def write_tfrecord(dirs, out):
+    with tf.python_io.TFRecordWriter(out) as writer:
+        example_buffer = []
+        for i, dir in enumerate(dirs):
+            example_buffer.extend(extract_segment(dir))
+            print("extracted {} segments".format(i))
+
+            if len(example_buffer) > 500000:
+                random.shuffle(example_buffer)
+                for e in example_buffer:
+                    writer.write(e)
+                example_buffer = []
+
+                print("\n\nflushed the buffer\n\n")
+
+        random.shuffle(example_buffer)
+        for e in example_buffer:
+            writer.write(e)
+
+
+segments = glob.glob("/mnt/Bulk Storage/commaai/comma2k19/*/*/*")
+random.shuffle(segments)
+
+train = segments[:1933]
+validation = segments[1933:]
+
+write_tfrecord(train, "/mnt/Bulk Storage/commaai/monolithic_train.tfrecord")
+write_tfrecord(validation, "/mnt/Bulk Storage/commaai/monolithic_validation.tfrecord")
