bfbbaad785b31fe96240fbd8ed7019d21333e8ff
Lucas Sloan <lucas.sloan@gmail.com>
2019-12-10 17:30:03 -0800

    Include the forward and backward versions in the same batch.

--- loader.py
+++ loader.py
@@ -20,23 +20,31 @@ def parse_record(tfrecord, training):
     frame_one = decode_and_process_frame(proto['frame_one'], training)
     frame_two = decode_and_process_frame(proto['frame_two'], training)
 
-    if (not training) or tf.random.uniform([]) > 0.5:
-        image = tf.concat((frame_one, frame_two), axis=2)
-        position = proto['position']
-        orienation = proto['orientation']
-    else:
-        image = tf.concat((frame_two, frame_one), axis=2)
-        position = -1 * proto['position']
-        orienation = -1 * proto['orientation']
+    image = tf.concat((frame_one, frame_two), axis=2)
+    position = proto['position']
+    orienation = proto['orientation']
 
-    return image, position, orienation, proto['speed']
+    # if not training:
+    #     tf.data.Dataset.from_tensors([image, position, orienation, proto['speed']])
+
+    rev_image = tf.concat((frame_two, frame_one), axis=2)
+    rev_position = -1 * proto['position']
+    rev_orientation = -1 * proto['orientation']
+    
+    images = tf.stack((image, rev_image))
+    positions = tf.stack((position, rev_position))
+    orientations = tf.stack((orienation, rev_orientation))
+    speeds = tf.stack((proto['speed'], proto['speed']))
+
+    return tf.data.Dataset.from_tensor_slices((images, positions, orientations, speeds))
 
 def load_tfrecord(filename, training):
     raw_dataset = tf.data.TFRecordDataset(filename)
 
     dataset = raw_dataset.map(lambda x: parse_record(x, training), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    if training:
-        dataset = dataset.shuffle(15000)
+    dataset = dataset.flat_map(lambda x: x)
+    # if training:
+    #     dataset = dataset.shuffle(15000)
     dataset = dataset.batch(200)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     return dataset
