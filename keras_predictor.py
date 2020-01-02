5a0c2e3628bbc878b7cfe04810355721baf23667
Lucas Sloan <lucas.sloan@gmail.com>
2020-01-01 18:28:03 -0800

    Define the model using the functional api.

--- keras_predictor.py
+++ keras_predictor.py
@@ -4,30 +4,31 @@ import loader
 training_dataset = loader.load_tfrecord("/mnt/Bulk/commaai/monolithic_train.tfrecord", True)
 validation_dataset = loader.load_tfrecord("/mnt/Bulk/speedchallenge/monolithic_test.tfrecord", False)
 
-model = tf.keras.Sequential()
+inputs = tf.keras.Input(shape=(128, 416, 6))
 
 # encoder
-model.add(tf.keras.layers.Conv2D(32, (7, 7), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.BatchNormalization())
+conv1 = tf.keras.layers.Conv2D(32, (7, 7), strides=(2, 2), padding='same', activation=tf.nn.relu)(inputs)
+conv1 = tf.keras.layers.BatchNormalization()(conv1)
+conv2 = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv1)
+conv2 = tf.keras.layers.BatchNormalization()(conv2)
+conv3 = tf.keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv2)
+conv3 = tf.keras.layers.BatchNormalization()(conv3)
+conv4 = tf.keras.layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv3)
+conv4 = tf.keras.layers.BatchNormalization()(conv4)
+conv5 = tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv4)
+conv5 = tf.keras.layers.BatchNormalization()(conv5)
 
 # thingy
-model.add(tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu))
-model.add(tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu))
+conv6 = tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv5)
+conv7 = tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same', activation=tf.nn.relu)(conv6)
 
 # pose extractor
-model.add(tf.keras.layers.Conv2D(6, (1, 1), padding='valid'))
-model.add(tf.keras.layers.Lambda(lambda x: tf.reduce_mean(x, [1, 2])))
-model.add(tf.keras.layers.Reshape((6,)))
-model.add(tf.keras.layers.Lambda(lambda x: tf.concat([x[:, :3] * 0.01, x[:, 3:6] * 0.001], axis=1)))
+pose = tf.keras.layers.Conv2D(6, (1, 1), padding='valid')(conv7)
+pose = tf.keras.layers.Lambda(lambda x: tf.reduce_mean(x, [1, 2]))(pose)
+pose = tf.keras.layers.Reshape((6,))(pose)
+outputs = tf.keras.layers.Lambda(lambda x: tf.concat([x[:, :3] * 0.01, x[:, 3:6] * 0.001], axis=1))(pose)
 
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=['mse'])
 
-model.fit(training_dataset, epochs=10, validation_data=validation_dataset)
+model.fit(training_dataset, epochs=10)
