db80aa61eb9b7e4fc95af0d5de2b26faf975b42b
Lucas Sloan <lucas.sloan@gmail.com>
2020-01-15 22:54:06 -0800

    Get the dataset_test script to work with a dataset returning dicts.

--- dataset_test.py
+++ dataset_test.py
@@ -4,11 +4,14 @@ import loader
 
 tf.enable_eager_execution()
 
-training_dataset = loader.load_tfrecord("D:\\commaai\\monolithic_train.tfrecord", True)
+training_dataset = loader.load_tfrecord("D:\\commaai\\monolithic_train.tfrecord", 20, True)
 
-for frames, positions, orienations, speeds in training_dataset:
-    for i, f in enumerate(frames[:20]):
-        imageio.imsave("test/frame{}.jpg".format(i), f)
-    for p in positions[:20]:
+for batch in training_dataset:
+    # print(batch[1]['pose'])
+    for p in batch[1]['pose']:
         print(p)
+    # for i, f in enumerate(frames[:20]):
+    #     imageio.imsave("test/frame{}.jpg".format(i), f)
+    # for p in positions[:20]:
+    #     print(p)
     break
