e3d8b7577dcf2fd608d07e5c6b2d7af571065a09
Lucas Sloan <lucas.sloan@gmail.com>
2020-01-19 21:30:35 -0800

    Use a version of the validation data that enables multi framerate.

--- keras_predictor.py
+++ keras_predictor.py
@@ -7,7 +7,7 @@ BATCH_SIZE = 200
 mirrored_strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
 with mirrored_strategy.scope():
     training_dataset = loader.load_tfrecord("/mnt/Bulk/commaai/monolithic_train.tfrecord", BATCH_SIZE, True)
-    validation_dataset = loader.load_tfrecord("/mnt/Bulk/speedchallenge/monolithic_test.tfrecord", BATCH_SIZE, False)
+    validation_dataset = loader.load_tfrecord("/mnt/Bulk/speedchallenge/monolithic_multi_framerate.tfrecord", BATCH_SIZE, False)
 
     inputs = tf.keras.Input(shape=(128, 416, 6), name='frames')
 
